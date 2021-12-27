6249aed57828254e029357baccc3c07e4e1ff9f4
Dimik151 <dmitry_zvorigin@mail.ru>
2021-12-27 21:08:38 +0500

    'main'

---
+++ task_15/task_15_handler.php
@@ -0,0 +1,54 @@
+<?php 
+
+// email = zigzag@mail 
+// password = 123456
+
+session_start();
+
+$user_name = 'root';
+$user_pass = '';
+
+try {
+    $db = new PDO('mysql:host=localhost;dbname=tasks', $user_name, $user_pass);
+} catch (PDOException $e) {
+    print "Error!: " . $e->getMessage() . "<br/>";
+    exit();
+}
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST'){
+    $auth = veritify_user();
+    if ($auth){
+        $_SESSION['login'] = $auth;
+        header('Location: task_15_logout.php');
+    }else{
+        $_SESSION['err'] = 'Неверный логин или пароль';
+        header('Location: task_15.php');
+    }
+}
+
+
+if (isset($_GET['exit'])){
+    unset($_SESSION['login']);
+    header('Location: task_15.php');
+}
+
+function veritify_user() {
+    global $db;
+    // $errros = '';
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+
+    $stmt = $db->prepare('SELECT * FROM login WHERE email = :email');
+    $stmt->execute(['email' => $email]);
+    $user = $stmt->fetch();
+    if (!$user){
+        // $errros = "Неверное имя или пароль";
+    }else{
+        if (!password_verify($password, $user['password'])){
+            // $errros = "Неверный пароль";
+        }else{
+            return $user['email'];
+        }
+    }
+    return FALSE;
+}
